41d737e963ac8f1cd8fe744d6adbceb9bbfa86c5
Olisakwe George <olisakwegeorgejunior@gmail.com>
2025-11-22 15:37:48 +0100

    Edits on second

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFoundScreen() {
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
+        <ThemedText type="title">This screen does not exist yet.</ThemedText>
         <Link href="/" style={styles.link}>
           <ThemedText type="link">Go to the home screen and try to fix whatever the issue is!</ThemedText>
         </Link>
