a604dc96222be0d77d94b4f0a614dd5bb9af5674
George Olisakwe <olisakwegeorgejunior@gmail.com>
2025-12-23 12:05:17 +0100

    or

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -11,7 +11,7 @@ export default function NotFoundScreen() {
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist.</ThemedText>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to the Home Screen and try to fix whatever the issue is (if theres any)!</ThemedText>
+          <ThemedText type="link">I&apos()m changing this text and it will be like this until further notice.</ThemedText>
         </Link>
       </ThemedView>
     </>
