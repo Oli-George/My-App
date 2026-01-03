2a01bf190c7c762998e6598efbb2f88c2a61f893
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-01-03 21:22:11 +0100

    Third day of Jan. Did an actual LeetCode with Fortune. Need to work more on other things

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -87,7 +87,7 @@ export default function TabTwoScreen() {
           ios: (
             <ThemedText>
               The <ThemedText type="defaultSemiBold">components/ParallaxScrollView.tsx</ThemedText>{' '}
-              component provides a parallax effect for the header image.
+              component provides a parallax effect for the header image. Cool, isn't it?
             </ThemedText>
           ),
         })}
