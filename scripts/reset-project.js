e92a70c32b9ce47276f8f20e46af26f624e3a76f
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-01-22 22:51:14 +0100

    Failed to reset

--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -91,7 +91,7 @@ const moveDirectories = async (userInput) => {
         userInput === "y"
           ? `\n3. Delete the /${exampleDir} directory when you're done referencing it.`
           : ""
-      }`
+      }`,
     );
   } catch (error) {
     console.error(`❌ Error during script execution: ${error.message}`);
@@ -105,8 +105,8 @@ rl.question(
     if (userInput === "y" || userInput === "n") {
       moveDirectories(userInput).finally(() => rl.close());
     } else {
-      console.log("❌ Invalid input. Please enter 'Y' or 'N'.");
+      console.log("❌ Invalid input. Please enter 'Y' or 'N'. ");
       rl.close();
     }
-  }
+  },
 );
