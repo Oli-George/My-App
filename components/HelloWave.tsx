298766b5bfe16b56b0b1e3f689857bd8b0cf0412
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-01-22 22:49:16 +0100

    Wave addition

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -26,7 +26,7 @@ export function HelloWave() {
 
   return (
     <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
+      <ThemedText style={styles.text}>👋😎</ThemedText>
     </Animated.View>
   );
 }
@@ -40,3 +40,5 @@ const styles = StyleSheet.create({
     marginTop: -6,
   },
 });
+
+
