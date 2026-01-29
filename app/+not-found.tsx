0be325dd0039881b1aa1a333c4fdf5c62c0ffd2f
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-01-29 20:30:47 +0100

    Corrected an earlier mistake of mine.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,7 +2,7 @@ import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {  ThemedView  } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
@@ -12,7 +12,7 @@ export default function NotFoundScreen() {
         <ThemedText type="title">This screen does not exist yet.I&apos()ll have to create it.</ThemedText>
         <Link href="/" style={styles.link}>
           <ThemedText type="link">I&apos()m changing this text and it will be like this until further notice. I need more inspiration.</ThemedText>
-          <ThemedText>Try the linnk I corrected earlier on. How&apos()s it? Does it work?</ThemedText>
+          <ThemedText>Try the link I corrected earlier on. How&apos()s it? Does it work?</ThemedText>
         </Link>
       </ThemedView>
     </>
