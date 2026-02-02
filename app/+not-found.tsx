46328ccfdc89fb5f147807008e6e747adbb0eedd
George <olisakwegeorgejunior@gmail.com>
2026-02-02 21:06:08 +0100

    Leetcode been crazy recently but we keep going

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -6,7 +6,7 @@ import {  ThemedView  } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
-    <>
+     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist yet.I&apos()ll have to create it.</ThemedText>
