e565f86f2718ddd199e12be1780a991e00619f69
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-02-08 20:51:23 +0100

    Good night, and good luck for tomorrow.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFoundScreen() {
         <ThemedText type="title">This screen does not exist yet.I&apos()ll have to create it.</ThemedText>
         <Link href="/" style={styles.link}>
           <ThemedText type="link">I&apos()m changing this text and it will be like this until further notice. I need more inspiration.</ThemedText>
-          <ThemedText>Try the link I corrected earlier on. How&apos()s it? Does it work?</ThemedText>
+          <ThemedText>Try the link I corrected earlier on. How&apos()s it? Does it work?</ThemedText>  
         </Link>
       </ThemedView>
     </>
@@ -21,13 +21,13 @@ export default function NotFoundScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
   },
   link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+    marginTop: 15, 
+    paddingVertical: 15, 
+  }, 
 });
