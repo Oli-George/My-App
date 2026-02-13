1f640522763b101bb30bb5bb68af049c66a15895
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-02-13 22:06:57 +0100

    Last day before the end of 200L first semester finals, which is coincindentally Valentines.

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -2,32 +2,33 @@ import { Link, Stack } from 'expo-router';
 import { StyleSheet } from 'react-native';
 
 import { ThemedText } from '@/components/ThemedText';
-import {  ThemedView  } from '@/components/ThemedView';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
   return (
-     <>
+    <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <ThemedView style={styles.container}>
         <ThemedText type="title">This screen does not exist yet.I&apos()ll have to create it.</ThemedText>
         <Link href="/" style={styles.link}>
           <ThemedText type="link">I&apos()m changing this text and it will be like this until further notice. I need more inspiration.</ThemedText>
-          <ThemedText>Try the link I corrected earlier on. How&apos()s it? Does it work?</ThemedText>  
+          <ThemedText>Try the link I corrected earlier on. How&apos()s it? Does it work?</ThemedText>
         </Link>
       </ThemedView>
     </>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center' ,
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
   },
   link: {
-    marginTop: 15, 
-    paddingVertical: 15, 
-  },  //Yeahhhhhh.
+    marginTop: 15,
+    paddingVertical: 15,
+  },  //Yeahhhh.
 });
