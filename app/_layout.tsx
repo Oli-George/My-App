5492355e095076ee65167581f13f3ab5f990c916
George <olisakwegeorgejunior@gmail.com>
2026-03-27 22:19:51 +0100

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,23 +3,23 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/useColorScheme';   
 
 // This is the root layout of the app. It wraps all the pages and components. I'll delete this file if I don't need it.
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  }); 
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),  
+  });  
 
 
   if (!loaded) {
     // Async font loading only occurs in development. Which is nice. And I like it like it that way.
     return null;
-  }
+  }   
 
-  
+ 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
@@ -30,7 +30,6 @@ export default function RootLayout() {
     </ThemeProvider>
   );
 
-  //Laptop is back!!! So happy man frfr. Been 6 weeks now.
 
 }
 
