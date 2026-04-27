1483fbbe01e53900551270eb83d1df248fe24cae
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-04-27 22:37:21 +0100

    So I'll correct the spacing here.

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -23,5 +23,4 @@ export function ExternalLink({ href, ...rest }: Props) {
   );
 }
 
-
 //What's an async/await function exactly?? I seem to have a bad headache this night. 
--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -11,6 +11,7 @@ import { ThemedView } from '@/components/ThemedView';
 import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
+
 const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<{
