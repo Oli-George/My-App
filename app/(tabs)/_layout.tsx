76e795ba8047a8c0f49fd5a4272f1e7d2a8e477d
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-05-05 21:29:06 +0100

    Tireddddddd

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,10 +30,10 @@ export default function TabLayout() {
       }}>
       <Tabs.Screen
         name="index"
-        options={{
+        options={{  
           title: 'Home',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+        } }
       />
 
       <Tabs.Screen
