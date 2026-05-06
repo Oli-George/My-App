353d088ef7fa5f7df606b5b476fbc7e21a954155
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-05-06 21:45:32 +0100

    Still not feeling not so good.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout() {
 
   if (!loaded) {
     // Async font loading only occurs in development. Which is nice. And I like it like it that way.
-    return null;
+    return null; 
   }
 
   return (
