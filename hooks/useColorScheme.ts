9751c8a97fbf566680f71951bd31b9182af34e70
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-05-29 21:19:36 +0100

    Been a while since I was here.

--- hooks/useColorScheme.ts
+++ hooks/useColorScheme.ts
@@ -1,4 +1,2 @@
 export { useColorScheme } from 'react-native';
 
-// Note: This file is necessary to work around a known issue with the Metro bundler. 
-// See, it works well. No need to worry about anything really serious (for now).
