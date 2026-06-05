867846ec0d5ceab18ef7ae970edd94651b007bbd
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-06-05 23:48:52 +0100

    Nothing tosee here.

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -10,7 +10,6 @@ type IconSymbolName = keyof typeof MAPPING;
 /**
  * Add your SF Symbols to Material Icons mappings here.
  * - see Material Icons in the [Icons Directory](https://icons.expo.fyi).
- * - see SF Symbols in the [SF Symbols](https://developer.apple.com/sf-symbols/) app.
  */
 
 const MAPPING = {
@@ -24,6 +23,7 @@ const MAPPING = {
  * An icon component that uses native SF Symbols on iOS, and Material Icons on Android and web.
  * This ensures a consistent look across platforms, and optimal resource usage. 
  * Icon `name`s are based on SF Symbols and require manual mapping to Material Icons.
+ * 
  */
 
 export function IconSymbol({
