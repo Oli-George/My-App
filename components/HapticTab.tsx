8dc80ac328e2718e55a1241af9abbf183c462089
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-06-11 22:54:34 +0100

    Rien

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -20,4 +20,4 @@ export function HapticTab(props: BottomTabBarButtonProps) {
 export default HapticTab;
 
 
-//Haptic Tab. What does it even do?? Still no idea.
+//Haptic Tab. What does it even do?? Still no idea. Toujours rien.
