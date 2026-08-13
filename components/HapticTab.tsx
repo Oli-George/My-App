8c6df2440ef2aeb6c55b2ab5e3ef0052c6628768
George Olisakwe <olisakwegeorgejunior@gmail.com>
2026-08-13 22:14:04 +0100

    Mon nez n'arrete pas de couler.

--- components/HapticTab.tsx
+++ components/HapticTab.tsx
@@ -1,6 +1,7 @@
 import { BottomTabBarButtonProps } from '@react-navigation/bottom-tabs';
 import { PlatformPressable } from '@react-navigation/elements';
 import * as Haptics from 'expo-haptics';
+import plot_confusion from 'app\(tabs)\mehh.ipynb'
 
 export function HapticTab(props: BottomTabBarButtonProps) {
   return (
